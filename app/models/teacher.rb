277595f6f82b82249a3d412ef5ec0be869c2f872
Jesseract <jlpearce@gmail.com>
2015-02-08 15:26:21 -0500

    Add validation for email and passwork #shipit

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,4 +2,12 @@ class Teacher < ActiveRecord::Base
   has_secure_password
   has_many :parents
   has_many :students
+
+  validates :password, length: { maximum: 30 }
+  validates_confirmation_of :password
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+
 end
