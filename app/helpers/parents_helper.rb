1fdec79da441cf20ccd956c117cab69b93419292
Ashley Ellis <anellis12@gmail.com>
2015-02-07 20:51:06 -0500

    Add parents index page

--- app/helpers/parents_helper.rb
+++ app/helpers/parents_helper.rb
@@ -1,2 +1,19 @@
 module ParentsHelper
+  def all_parents
+    result = "<table>"
+    if session[:user_type] == "teacher"
+      teacher= Teacher.find(session[:user_id])
+      teacher.parents.each do |p|
+        result << "<tr>"
+        result << "<td>#{link_to p.parent_name, edit_parent_path(p)}</td>"
+        result << "</tr>"
+      end
+      result << "</table>"
+    elsif session[:user_type] == "student"
+      redirect_to root_path, notice: "You do not have permission to view that page"
+    elsif session[:user_type] == "parent"
+      redirect_to root_path, notice: "You do not have permission to view that page"
+    end
+    result.html_safe
+  end
 end
